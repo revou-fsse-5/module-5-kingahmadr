2260203dd9400b677f3bc7c3708a87fa04c773aa
kingahmadr <ahmadnromiz@gmail.com>
2024-09-19 06:35:47 +0700

    checkpoint

--- src/app/Loading.tsx
+++
@@ -1,8 +0,0 @@
-export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
-  return (
-    <div className="flex flex-col bottom-0 absolute text-2xl">
-      <p>Loading ...</p>
-    </div>
-  );
-}
--- src/app/pages/products/page.tsx
+++
@@ -1,5 +0,0 @@
-// import Image from "next/image";
-import CategoryTab from "../../components/CategoryTab";
-export default function Home() {
-  return <CategoryTab />;
-}
