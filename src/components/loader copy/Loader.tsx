4b1ecdfbd4576d872b04505b13de0a5f513f30dc
kingahmadr <ahmadnromiz@gmail.com>
2024-09-20 22:52:01 +0700

    test create folder

---
+++ src/components/loader copy/Loader.tsx
@@ -0,0 +1,29 @@
+// "use client";
+// import { RotatingLines, ProgressBar } from "react-loader-spinner";
+// const Loader = () => {
+//   const RotatingLoader = (
+//     <RotatingLines
+//       visible={true}
+//       width="96"
+//       strokeWidth="5"
+//       animationDuration="0.75"
+//       ariaLabel="rotating-lines-loading"
+//     />
+//   );
+
+//   const ProgressLoader = (
+//     <ProgressBar
+//       visible={true}
+//       height="80"
+//       width="80"
+//       ariaLabel="progress-bar-loading"
+//     />
+//   );
+
+//   return {
+//     RotatingLoader,
+//     ProgressLoader,
+//   };
+// };
+
+// export default Loader;
